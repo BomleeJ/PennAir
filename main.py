073e622dec4a83a621de7b4e39c95fd9ff150711
OmarJ <omarkjingo@gmail.com>
2025-09-06 18:10:02 -0400

    Phase 3

---
+++ main.py
@@ -0,0 +1,55 @@
+import numpy as np
+import cv2 as cv
+from src.CountourDrawer import add_contours_to_frame
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def analyze_image(result_name: str, image_path: str = "files/Grassy.png"):
+    result_path = f"results/{result_name}.png"
+    img = cv.imread(cv.samples.findFile(f"{image_path}"))
+    processedImage = add_contours_to_frame(img, False)
+    cv.imwrite(result_path, processedImage)
+
+def analyze_video(result_name: str, video_path: str):
+    result_path = f"results/{result_name}.avi"
+    cap = cv.VideoCapture(video_path)
+
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    FPS = 20.0
+    WIDTH = 1920
+    HEIGHT = 1080
+
+    out = cv.VideoWriter(result_path, fourcc, FPS, (WIDTH, HEIGHT), isColor=True)
+    val = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        logging.info(f"Processing Frame {val} from video {video_path}")
+        val += 1
+        processed_frame = add_contours_to_frame(frame)
+        out.write(processed_frame)
+
+    cap.release()
+    out.release()
+
+def main():
+    Phase2_video_path = "files/Phase2.mp4"
+    Phase3_video_path = "files/Phase3.mp4"
+    
+    analyze_image("Grassy_with_contours")
+    logging.info("Image Complete!")
+    """
+    analyze_video("Phase2_with_contours2.avi", Phase2_video_path)
+    logging.info("Video1 Complete!")
+    
+    analyze_video("Phase3_with_contours2.avi", Phase3_video_path)
+    logging.info("Video2 Complete!")
+    """
+    
+
+if __name__ == "__main__":
+    main()
+
--- src/CountourDrawer.py
+++ src/CountourDrawer.py
@@ -1,11 +1,11 @@
 from typing import List, Optional
-from CustomTypes import Image, Contour
+from src.CustomTypes import Image, Contour
 import matplotlib.pyplot as plt
 import cv2 as cv
+import numpy as np
 
 
-
-def add_contours_to_frame(frame: Image):
+def add_contours_to_frame(frame: Image, Video=True):
     """
     Apply's Contours around Shapes to a given Frame
 
@@ -15,63 +15,86 @@ def add_contours_to_frame(frame: Image):
     Returns:
         processed_frame: Image(Alias for np.ndarray)
     """
-    import time
 
-    
     grayscale = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     adaptiveThresholdImage = apply_adaptive_thresholding(grayscale)
-    
-    SansNoise = eliminate_noise(adaptiveThresholdImage)
-    
-    contours = calculate_countours(SansNoise)
+
+    SansNoise = eliminate_noise(adaptiveThresholdImage, Video)
+
+    contours = calculate_countours(SansNoise, Video)
 
     centroids = calculate_centroids(contours)
 
     draw_centroids(centroids, frame)
     ImageWithContours = cv.drawContours(frame, contours, -1, (255, 255, 0), 3)
-    
+
+    images = [grayscale, adaptiveThresholdImage, SansNoise, ImageWithContours]
     return ImageWithContours
+
 
 def apply_adaptive_thresholding(frame: Image) -> Image:
     """
-    For every Pixel, The mean is calculated over a block of odd size. 
-    The mean is then subtracted by the constant C. If the pixel intensity is lower 
-    than the mean - C is set to 0, otherwise it is set to 255. 
+    For every Pixel, The mean is calculated over a block of odd size.
+    The mean is then subtracted by the constant C. If the pixel intensity is lower
+    than the mean - C is set to 0, otherwise it is set to 255.
     """
 
     Constant = 2
     BlockSize = 9
 
-    adaptive_gaussian = (
-        cv.adaptiveThreshold(frame, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, 
-                             cv.THRESH_BINARY, BlockSize, Constant)
+    adaptive_gaussian = cv.adaptiveThreshold(
+        frame, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, BlockSize, Constant
     )
 
-    return adaptive_gaussian    
+    return adaptive_gaussian
     pass
 
-def eliminate_noise(frame: Image):
+
+def eliminate_noise(frame: Image, video: bool):
     """
     This applies a Gaussian Blur to the Image and only keeps the
     Very Bright Areas ig
     """
-    blur = cv.blur(frame, (9, 9))
-    ret, thresh = cv.threshold(blur, 205, 255, cv.THRESH_BINARY) 
-    
+    if video:
+        iter = 5
+        Dilatekernel = np.ones((5, 5), np.uint8)
+        blurKernel = (9, 9)
+    else:
+        iter = 2
+        Dilatekernel = np.ones((3, 3), np.uint8)
+        blurKernel = (5, 5)
+
+    thresh = frame
+    kernel = np.ones((5, 5), np.uint8)
+    for i in range(iter):
+        blur = cv.blur(thresh, blurKernel)
+        ret, thresh = cv.threshold(blur, 205, 255, cv.THRESH_BINARY)
+    thresh = cv.dilate(thresh, Dilatekernel, iterations=5)
     return thresh
 
-def calculate_countours(frame: Image):
+
+def calculate_countours(frame: Image, video):
+    if video:
+        Areathres = 5000
+    else:
+        Areathres = 500
     contours, hierarchy = cv.findContours(frame, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    return contours
+    areas = [cv.contourArea(cnt) for cnt in contours]
+    pairs = list(zip(areas, contours))
+    pairs = list(filter(lambda x: x[0] > Areathres, pairs))
+    pairs.sort(key=lambda x: x[0], reverse=True)
+    ret = [x[1] for x in pairs[:5]]
+    return ret
+
 
 def calculate_centroids(contours):
     centroids = []
     for contour in contours:
         M = cv.moments(contour)
         try:
-            cx = int(M['m10']/M['m00'])
-            cy = int(M['m01']/M['m00'])
+            cx = int(M["m10"] / M["m00"])
+            cy = int(M["m01"] / M["m00"])
         except ZeroDivisionError:
             continue
         coord = (cx, cy)
@@ -79,34 +102,23 @@ def calculate_centroids(contours):
 
     return centroids
 
+
 def draw_centroids(centroids, frame):
-    WHITE = (255,255,255)
+    WHITE = (255, 255, 255)
     font = cv.FONT_HERSHEY_SIMPLEX
     font_size = 0.5
     font_thickness = 1
     radius = 5
     for center in centroids:
         cx, cy = center
-        cv.putText(frame, f'center {cx, cy}',(cx + 40 ,cy), font, font_size, WHITE, font_thickness,cv.LINE_AA)
+        cv.putText(
+            frame,
+            f"center {cx, cy}",
+            (cx + 40, cy),
+            font,
+            font_size,
+            WHITE,
+            font_thickness,
+            cv.LINE_AA,
+        )
         cv.circle(frame, center, radius, WHITE, -1)
-    
-
-def plot_images(images: List[Image], titles: Optional[List[str]] = None):
-    """
-    Plot a list of Black and White images with optional titles.
-
-    Args:
-        images: List of images to plot.
-        titles: List of titles for each image.
-    """
-    if not titles:
-        titles = [f"Image {i}" for i in range(len(images))]
-
-    cols = min(len(images), 4)
-    rows = (len(images) + cols - 1) // cols
-    for i in range(len(images)):
-        plt.subplot(rows, cols, i+1)
-        plt.imshow(images[i],'gray')
-        plt.title(titles[i])
-        plt.xticks([]),plt.yticks([])
-    plt.show()
